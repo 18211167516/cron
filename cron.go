4b6aec31cbc211895218115f03f7b074bf86b218
=baichonghua <18211167516@163.com>
2020-10-27 15:31:21 +0800

    init v1

--- cron.go
+++ cron.go
@@ -3,7 +3,7 @@ package cron
 import (
 	"sync"
 	"time"
-	"fmt"
+	"sort"
 )
 
 // TaskID is Task ID
@@ -16,6 +16,25 @@ type Task struct {
 	Job      Job
 	Next     time.Time
 	Prev     time.Time
+}
+
+// byTime is a wrapper for sorting the entry array by time
+// (with zero time at the end).
+type byTime []*Task
+
+func (s byTime) Len() int      { return len(s) }
+func (s byTime) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s byTime) Less(i, j int) bool {
+	// Two zero times should return false.
+	// Otherwise, zero is "greater" than any other time.
+	// (To sort it at the end of the list.)
+	if s[i].Next.IsZero() {
+		return false
+	}
+	if s[j].Next.IsZero() {
+		return true
+	}
+	return s[i].Next.Before(s[j].Next)
 }
 
 // Cron struct
@@ -47,7 +66,7 @@ type FuncJob func()
 
 func (f FuncJob) Run() { f() }
 
-func (c *Cron) addFunc(spec string,f func())(TaskID,error){
+func (c *Cron) AddFunc(spec string,f func())(TaskID,error){
 	return c.AddJob(spec, FuncJob(f))
 }
 
@@ -105,16 +124,86 @@ func (c *Cron) Start() {
 	go c.run()
 }
 
+func (c *Cron) startJob(j Job) {
+	go func() {
+		j.Run()
+	}()
+}
+
 // run 
 // step1 遍历全部task
 func (c *Cron) run(){
 	c.Info("Start")
 
-	//now := c.now()
-	//fmt.Println(now)
-	fmt.Printf("%v,%d\n",c.tasks,len(c.tasks))
+	now := c.now()
 	for _, task := range c.tasks {
-		c.Info("schedule", "now",task.ID)
+		task.Next = task.Schedule.Next(now)
+		c.Info("schedule", "now",now,"task",task.ID,"next",task.Next)
 	}
 
+	for {
+		// Determine the next entry to run.
+		sort.Sort(byTime(c.tasks))
+
+		var timer *time.Timer
+		if len(c.tasks) == 0 || c.tasks[0].Next.IsZero() {
+			// If there are no entries yet, just sleep - it still handles new entries
+			// and stop requests.
+			timer = time.NewTimer(100000 * time.Hour)
+		} else {
+			//获取下一次timer时间
+			timer = time.NewTimer(c.tasks[0].Next.Sub(now))
+		}
+
+		for {
+			select {
+			case now = <-timer.C:
+				now = now.In(c.location)
+				c.logger.Info("wake", "now", now)
+
+				// Run every entry whose next time was less than now
+				for _, e := range c.tasks {
+					//下次执行时间未到或者是零点
+					if e.Next.After(now) || e.Next.IsZero() {
+						break
+					}
+					c.startJob(e.Job)
+					e.Prev = e.Next
+					e.Next = e.Schedule.Next(now)
+					c.logger.Info("run", "now", now, "entry", e.ID, "next", e.Next)
+				}
+
+			case newEntry := <-c.add:
+				timer.Stop()
+				now = c.now()
+				newEntry.Next = newEntry.Schedule.Next(now)
+				c.tasks = append(c.tasks, newEntry)
+				c.logger.Info("added", "now", now, "entry", newEntry.ID, "next", newEntry.Next)
+
+			case <-c.stop:
+				timer.Stop()
+				c.logger.Info("stop")
+				return
+
+			case id := <-c.remove:
+				timer.Stop()
+				now = c.now()
+				c.removeTask(id)
+				c.logger.Info("removed", "entry", id)
+			}
+
+			break
+		}
+	}
+}
+
+// removeTask 移除task
+func (c *Cron) removeTask(id TaskID) {
+	var entries []*Task
+	for _, e := range c.tasks {
+		if e.ID != id {
+			entries = append(entries, e)
+		}
+	}
+	c.tasks = entries
 }
--- example/go.mod
+++ example/go.mod
@@ -1,3 +1,7 @@
 module github.com/18211167516/cron/example
 
 go 1.14
+
+require github.com/18211167516/cron v0.0.0-20201027061527-88e96a816084
+
+replace github.com/18211167516/cron => ../../cron
